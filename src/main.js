d66b6a787a6ec2f5506642e32a52c1bebf560728
dvasilchuk <dvasilchuk@phoenixit.ru>
2020-07-31 11:28:45 +0300

    Исправляет функции отрисовки компонентов

--- src/main.js
+++ src/main.js
@@ -5,18 +5,14 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const createUserAvatarTemplate = () => {
-  return (`
-  <section class="header__profile profile">
+const createUserAvatarTemplate = () =>
+  `<section class="header__profile profile">
     <p class="profile__rating">Movie Buff</p>
     <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-  </section>
-  `);
-};
+  </section>`;
 
-const createMainNavigationTemplate = () => {
-  return (`
-  <nav class="main-navigation">
+const createMainNavigationTemplate = () =>
+  `<nav class="main-navigation">
     <div class="main-navigation__items">
       <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
       <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
@@ -24,64 +20,45 @@ const createMainNavigationTemplate = () => {
       <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
     </div>
     <a href="#stats" class="main-navigation__additional">Stats</a>
-  </nav>
-  `);
-};
+  </nav>`;
 
-const createSortTemplate = () => {
-  return (`
-  <ul class="sort">
+const createSortTemplate = () =>
+  `<ul class="sort">
     <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
     <li><a href="#" class="sort__button">Sort by date</a></li>
     <li><a href="#" class="sort__button">Sort by rating</a></li>
-  </ul>
-  `);
-};
+  </ul>`;
 
-const createFilmsTemplate = () => {
-  return (`
-  <section class="films">
-    </section>
-  `);
-};
+const createFilmsTemplate = () =>
+  `<section class="films">
+    </section>`;
 
-const createFilmsListTemplate = () => {
-  return (`
-    <section class="films-list">
+const createFilmsListTemplate = () =>
+  `<section class="films-list">
       <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
       <div class="films-list__container">
       </div>
-    </section>
-  `);
-};
+    </section>`;
 
-const createShowMoreButton = () => {
-  return (`
-  <button class="films-list__show-more">Show more</button>
-  `);
-};
-const createFilmsListTopRatedTemplate = () => {
-  return (`
-    <section class="films-list--extra">
+const createShowMoreButton = () =>
+  `<button class="films-list__show-more">Show more</button>`;
+
+const createFilmsListTopRatedTemplate = () =>
+  `<section class="films-list--extra">
       <h2 class="films-list__title">Top rated</h2>
       <div class="films-list__container">
       </div>
-    </section>
-  `);
-};
-const createFilmsListMostCommentedTemplate = () => {
-  return (`
-  <section class="films-list--extra">
+    </section>`;
+
+const createFilmsListMostCommentedTemplate = () =>
+  `<section class="films-list--extra">
       <h2 class="films-list__title">Most commented</h2>
       <div class="films-list__container">
       </div>
-  </section>
-  `);
-};
+  </section>`;
 
-const createCardTemplate = () => {
-  return (`
-  <article class="film-card">
+const createCardTemplate = () =>
+  `<article class="film-card">
     <h3 class="film-card__title">The Dance of Life</h3>
     <p class="film-card__rating">8.3</p>
     <p class="film-card__info">
@@ -97,13 +74,10 @@ const createCardTemplate = () => {
       <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
       <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
     </form>
-  </article>
-  `);
-};
+  </article>`;
 
-const createPopUptemplate = () => {
-  return (`
-  <section class="film-details">
+const createPopUptemplate = () =>
+  `<section class="film-details">
   <form class="film-details__inner" action="" method="get">
     <div class="form-details__top-container">
       <div class="film-details__close">
@@ -271,9 +245,7 @@ const createPopUptemplate = () => {
       </section>
     </div>
   </form>
-</section>
-  `);
-};
+</section>`;
 
 const siteHeaderElement = document.querySelector(`.header`);
 render(siteHeaderElement, createUserAvatarTemplate(), `beforeend`);
