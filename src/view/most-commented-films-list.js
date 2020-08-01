9f3eb651d742a92b127ee436354ab823660d99f0
dvasilchuk <dvasilchuk@phoenixit.ru>
2020-08-02 00:34:44 +0300

    Разбивает проект на модули

---
+++ src/view/most-commented-films-list.js
@@ -0,0 +1,6 @@
+export const createFilmsListMostCommentedTemplate = () =>
+  `<section class="films-list--extra">
+      <h2 class="films-list__title">Most commented</h2>
+      <div class="films-list__container">
+      </div>
+  </section>`;
---
+++ src/view/top-rated-films-list.js
@@ -0,0 +1,6 @@
+export const createFilmsListTopRatedTemplate = () =>
+  `<section class="films-list--extra">
+      <h2 class="films-list__title">Top rated</h2>
+      <div class="films-list__container">
+      </div>
+    </section>`;
