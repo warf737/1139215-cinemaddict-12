dffc79e5a13ac759c5b95dadb6542dd6d0042311
dvasilchuk <dvasilchuk@phoenixit.ru>
2020-08-22 19:15:07 +0300

    Добавляет отрисовку для фильмов в доп категориях

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import {createCardTemplate} from "./view/card";
 import {createPopUptemplate} from "./view/popup";
 import {createShowMoreButton} from "./view/load-more-button";
 import {generateFilmCards} from "./mock/film-card";
-
+import {getRandomInteger} from "./utils";
 const FILMS_COUNT = 17;
 const CARDS_COUNT = 5;
 const ADDITIONAL_CARDS_COUNT = 2;
@@ -41,18 +41,22 @@ films.slice(0, displayingCardsCount).forEach((film) => render(
     filmsListContainerElement, createCardTemplate(film), `beforeend`)
 );
 
-render(filmsListElement, createShowMoreButton(), `beforeend`);
-
+// top rated
 render(filmsMainElement, createFilmsListTopRatedTemplate(), `beforeend`);
 
+// most commented
 render(filmsMainElement, createFilmsListMostCommentedTemplate(), `beforeend`);
 
+// button 'show more'
+render(filmsListElement, createShowMoreButton(), `beforeend`);
+
+// more films
 const additionalFilmsElement = filmsMainElement.querySelectorAll(`.films-list--extra`);
 
 additionalFilmsElement.forEach((element) => {
   let elementContainer = element.querySelector(`.films-list__container`);
   for (let i = 0; i < ADDITIONAL_CARDS_COUNT; i++) {
-    render(elementContainer, createCardTemplate(), `beforeend`);
+    render(elementContainer, createCardTemplate(films[getRandomInteger(0, films.length - 1)]), `beforeend`);
   }
 });
 
